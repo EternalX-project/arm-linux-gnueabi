22970b0a3af09a2dcfffebc57f1c772b77d820f8
Lawliet L <zexceed12300@gmail.com>
2021-08-08 00:47:54 +0000

    Update GCC 9 from CI build

---
+++ lib/gcc/arm-linux-gnueabi/9.4.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-multilib --disable-werror --with-pkgversion='EternalX GCC' CFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' CXXFLAGS='-g0 -O3 -fstack-protector-strong -fuse-linker-plugin -ffunction-sections -fdata-sections' --enable-languages=c,c++ --target=arm-linux-gnueabi --prefix=/drone/src/builder/arm-linux-gnueabi";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm10tdmi" }, { "tls", "gnu" } };
