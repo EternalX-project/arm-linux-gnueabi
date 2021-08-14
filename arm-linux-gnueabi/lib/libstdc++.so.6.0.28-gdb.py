45b9f8a6640d305992251d0406810096d4c3f8a9
Lawliet L <36181923+zexceed12300@users.noreply.github.com>
2021-08-14 09:00:51 +0000

    Update GCC 10 from CI build

--- arm-linux-gnueabi/lib/libstdc++.so.6.0.28-gdb.py
+++ arm-linux-gnueabi/lib/libstdc++.so.6.0.28-gdb.py
@@ -1,5 +1,5 @@
 # -*- python -*-
-# Copyright (C) 2009-2019 Free Software Foundation, Inc.
+# Copyright (C) 2009-2020 Free Software Foundation, Inc.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,7 +19,7 @@ import gdb
 import os
 import os.path
 
-pythondir = '/drone/src/builder/arm-linux-gnueabi/share/gcc-9.4.1/python'
+pythondir = '/drone/src/builder/arm-linux-gnueabi/share/gcc-10.3.1/python'
 libdir = '/drone/src/builder/arm-linux-gnueabi/arm-linux-gnueabi/lib'
 
 # This file might be loaded when there is no current objfile.  This
